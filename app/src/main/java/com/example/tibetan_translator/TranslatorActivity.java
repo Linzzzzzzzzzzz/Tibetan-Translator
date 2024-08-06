1506beb2783bd0de05327e43e203b96bfc02bec9
fyj <jacqueline_0521@163.com>
2024-08-06 16:41:09 +0800

    Android app initial

--- app/src/main/java/com/example/tibetan_translator/TranslatorActivity.java
+++ app/src/main/java/com/example/tibetan_translator/TranslatorActivity.java
@@ -2,23 +2,42 @@ package com.example.tibetan_translator;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class TranslatorActivity extends AppCompatActivity {
+    TextView language1;
+    TextView language2;
+
+    EditText inputText;
+    TextView outputText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_translator);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        language1 = (TextView) findViewById(R.id.language2);
+        language2 = (TextView) findViewById(R.id.language1);
+
+        inputText = (EditText) findViewById(R.id.inputText);
+        outputText = (TextView) findViewById(R.id.outputText);
+
+    }
+
+    public void language (View view) {
+        String tempText = language1.getText().toString();
+        language1.setText(language2.getText());
+        language2.setText(tempText);
+    }
+
+    public void translate(View view) {
+        outputText.setText(inputText.getText());
     }
 }
